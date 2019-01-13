46457fb36e93bd635323b52a5d7bc15c343dcc63
Ron DuPlain <ron.duplain@gmail.com>
2019-01-13 17:15:59 -0500

    Define tests with same name as code under test.

--- test/cljs/hello/unit/core.cljs
+++ test/cljs/hello/unit/core.cljs
@@ -1,14 +1,16 @@
 (ns hello.unit.core
-  (:require [cljs.test :refer-macros [is deftest]]
+  (:require [cljs.test :refer-macros [is deftest testing]]
             [hello.core :as core]))
 
-(deftest test-default-value
-  (is (= core/default ["world"])))
+(deftest default
+  (testing "value"
+    (is (= core/default ["world"]))))
 
-(deftest test-default
-  (is (= (with-out-str (core/-main))
-         "Hello, [34mworld[0m![0m\n")))
+(deftest -main
+  (testing "without arguments"
+    (is (= (with-out-str (core/-main))
+           "Hello, [34mworld[0m![0m\n")))
 
-(deftest test-with-args
-  (is (= (with-out-str (core/-main "one" "two" "three"))
-         "Hello, [34mone two three[0m![0m\n")))
+  (testing "with arguments"
+    (is (= (with-out-str (core/-main "one" "two" "three"))
+           "Hello, [34mone two three[0m![0m\n"))))
